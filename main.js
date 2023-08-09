00a6709a4e8af55781491998412d31de8a423e64
Guy187 <guylherme699@gmail.com>
2023-08-08 22:11:59 -0300

    Adicionei o index.html e main.js

---
+++ main.js
@@ -0,0 +1,76 @@
+// Pergunntar sobre esse teste para o Walguinão.
+// var carro = {
+//   modelo: "GM",
+//   portas: 4,
+//   Motor: "1.0",
+//   ano: 1998,
+//   cor: "vinnho",
+//   Descricao: function () {
+//     return
+//       "Carro modelo " +
+//       this.modelo +
+//       " cor " +
+//       this.cor +
+//       " ano " +
+//       this.ano +
+//       " possui motor " +
+//       this.Motor +
+//       " com " +
+//       this.portas +
+//       " portas"
+
+//   },
+// };
+// console.log(carro);
+
+ class Motor{
+   constructor(p){
+ this.p
+   }
+ }
+//NumeroDaEquipe = "Null"; Porque não tem um numero de equipe fixo definido.
+// No constructor passo parametros que todo o item da classe do objeto  deve ter. 
+
+
+  class Carro{
+    numeroDaEquipe = null;
+
+   constructor(cor,m){
+     this.cor = cor;
+     this.mbasic
+     this.motor = m;
+   }
+    acelerar(){
+      console.log("Vrummm")
+    }
+
+    trocarMotor(m){
+      this.motor = m;
+    }
+
+    setNumeroDaEquipe(n){
+      this.numeroDaEquipe = n;
+    }
+  }
+
+
+let mmaster = new Motor (100);
+let mmedio = new Motor (6 );
+  let mtop = new Motor (10);
+ let mbasic = new Motor(2);
+ 
+ let carFerrai = new Carro(null,mtop);
+ carFerrai.setNumeroDaEquipe(1);
+
+ let carBenetton = new Carro(null,mbasic);
+ carBenetton.setNumeroDaEquipe(2);
+
+ let carUnoComEscada = new Carro(vermelho,mmaster);
+carBenetton.carUnoComEscada.setNumeroDaEquipe(3);
+
+let carPejo = new Carro(verde,mmedio);
+ carPejo.setNumeroDaEquipe(4);
+ 
+
+
+
